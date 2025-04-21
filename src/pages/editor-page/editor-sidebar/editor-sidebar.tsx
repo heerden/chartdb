382a61aa46490100a7c8c901b88b92b8861ac1ee
Schalk van Heerden <schalk@heerden.com>
2025-04-21 22:16:41 +0200

    Resolve help

--- src/pages/editor-page/editor-sidebar/editor-sidebar.tsx
+++ src/pages/editor-page/editor-sidebar/editor-sidebar.tsx
@@ -2,7 +2,6 @@ import React, { useMemo } from 'react';
 import {
     Sidebar,
     SidebarContent,
-    SidebarFooter,
     SidebarGroup,
     SidebarGroupContent,
     SidebarHeader,
@@ -10,11 +9,9 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/sidebar/sidebar';
-import { Twitter, BookOpen } from 'lucide-react';
 import { SquareStack, Table, Workflow } from 'lucide-react';
 import { useLayout } from '@/hooks/use-layout';
 import { useTranslation } from 'react-i18next';
-import { DiscordLogoIcon } from '@radix-ui/react-icons';
 import { useBreakpoint } from '@/hooks/use-breakpoint';
 import ChartDBLogo from '@/assets/logo-light.png';
 import ChartDBDarkLogo from '@/assets/logo-dark.png';
@@ -69,35 +66,6 @@ export const EditorSidebar: React.FC<EditorSidebarProps> = () => {
         [selectSidebarSection, selectedSidebarSection, t, showSidePanel]
     );
 
-    const footerItems: SidebarItem[] = useMemo(
-        () => [
-            {
-                title: 'Discord',
-                icon: DiscordLogoIcon,
-                onClick: () =>
-                    window.open('https://discord.gg/QeFwyWSKwC', '_blank'),
-                active: false,
-            },
-            {
-                title: 'Twitter',
-                icon: Twitter,
-                onClick: () =>
-                    window.open(
-                        'https://x.com/intent/follow?screen_name=jonathanfishner',
-                        '_blank'
-                    ),
-                active: false,
-            },
-            {
-                title: 'Documentation',
-                icon: BookOpen,
-                onClick: () => window.open('https://docs.chartdb.io', '_blank'),
-                active: false,
-            },
-        ],
-        []
-    );
-
     return (
         <Sidebar
             side="left"
@@ -148,31 +116,6 @@ export const EditorSidebar: React.FC<EditorSidebarProps> = () => {
                     </SidebarGroupContent>
                 </SidebarGroup>
             </SidebarContent>
-
-            <SidebarFooter>
-                <SidebarMenu>
-                    {footerItems.map((item) => (
-                        <SidebarMenuItem key={item.title}>
-                            {item.badge && (
-                                <span className="absolute -right-1 -top-1 rounded-full bg-pink-500 px-[3px] py-px text-[8px] font-semibold text-white">
-                                    {item.badge}
-                                </span>
-                            )}
-                            <SidebarMenuButton
-                                className="hover:bg-gray-200 data-[active=true]:bg-gray-100 data-[active=true]:text-pink-600 data-[active=true]:hover:bg-pink-100 dark:hover:bg-gray-800 dark:data-[active=true]:bg-gray-900 dark:data-[active=true]:text-pink-400 dark:data-[active=true]:hover:bg-pink-950"
-                                isActive={item.active}
-                                asChild
-                                tooltip={item.title}
-                            >
-                                <button onClick={item.onClick}>
-                                    <item.icon />
-                                    <span>{item.title}</span>
-                                </button>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    ))}
-                </SidebarMenu>
-            </SidebarFooter>
         </Sidebar>
     );
 };
